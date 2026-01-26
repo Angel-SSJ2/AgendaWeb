b2b61672851c3a5831f6482ca5042b24fed05123
Angel-SSJ2 <arodriguez-2021698@kinal.edu.gt>
2026-01-25 20:49:02 -0600

    Update login button redirect to app.js

--- src/component/sections/login/Login.js
+++ src/component/sections/login/Login.js
@@ -19,7 +19,7 @@ let login = function() {
     button.innerHTML = "Iniciar Sesión";
 
     button.onclick = function(){
-        window.location.href= "../../src/app.html";
+        window.location.href= "../../src/app.js";
     }
 
     container.appendChild(h3);
